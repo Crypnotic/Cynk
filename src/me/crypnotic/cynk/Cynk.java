7f0a93d7bf24cd9e9415788c52c4f4d881b9f961
Crypnotic <crypnoticofficial@gmail.com>
2017-07-22 00:19:28 -0500

    Cynk v1.0.0

---
+++ src/me/crypnotic/cynk/Cynk.java
@@ -0,0 +1,44 @@
+package me.crypnotic.cynk;
+
+import java.io.File;
+
+public class Cynk {
+
+	public static void main(String[] arguments) {
+		if (arguments.length > 0) {
+			Cynk remover = new Cynk();
+
+			remover.init(arguments[0]);
+		} else {
+			System.out.println("Arguments required: [path]");
+		}
+	}
+
+	public void init(String name) {
+		File directory;
+		if (name.startsWith("./")) {
+			directory = new File(System.getProperty("user.dir"), name.substring(2));
+		} else {
+			directory = new File(name);
+		}
+
+		if (directory == null || !directory.exists() || !directory.isDirectory()) {
+			log("Unknown directory: " + name);
+			return;
+		}
+
+		DuplicateScanner scanner = new DuplicateScanner(directory);
+
+		log("Initializing scan for duplicate files in directory: " + directory.getAbsolutePath());
+
+		scanner.run();
+
+		log("Deleted " + scanner.getDeleted() + " duplicate files.");
+	}
+
+	public static void log(Object... values) {
+		for (Object value : values) {
+			System.out.println(value);
+		}
+	}
+}
---
+++ src/me/crypnotic/cynk/DuplicateScanner.java
@@ -0,0 +1,128 @@
+package me.crypnotic.cynk;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class DuplicateScanner implements Runnable {
+
+	private final File directory;
+	private final List<String> hashes;
+	private final Comparator<File> sort;
+	private int deleted;
+
+	public DuplicateScanner(File directory) {
+		this.directory = directory;
+		this.hashes = new ArrayList<String>();
+		this.deleted = 0;
+		
+		this.sort = (alpha, beta) -> {
+			if (alpha.isDirectory() && beta.isFile()) {
+				return 1;
+			} else if (alpha.isFile() && beta.isDirectory()) {
+				return -1;
+			} else {
+				return beta.getName().compareTo(alpha.getName());
+			}
+		};
+	}
+
+	public void run() {
+		reset();
+
+		List<File> duplicates = getDuplicates(directory);
+
+		duplicates.forEach(this::delete);
+
+		this.deleted = duplicates.size();
+	}
+
+	private List<File> getDuplicates(File directory) {
+		List<File> duplicates = new ArrayList<File>();
+
+		List<File> list = Arrays.asList(directory.listFiles());
+
+		Collections.sort(list, sort);
+
+		for (File file : list) {
+			if (file.isDirectory()) {
+				List<File> dirDuplicates = getDuplicates(file);
+
+				if (dirDuplicates.size() == file.listFiles().length) {
+					duplicates.add(file);
+				} else {
+					duplicates.addAll(dirDuplicates);
+				}
+			} else {
+				String checksum = checksum(file);
+
+				if (checksum == null) {
+					continue;
+				}
+
+				if (hashes.contains(checksum)) {
+					duplicates.add(file);
+				} else {
+					hashes.add(checksum);
+				}
+			}
+		}
+
+		return duplicates;
+	}
+
+	private String checksum(File file) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+
+			FileInputStream input = new FileInputStream(file);
+
+			int length;
+			byte[] buffer = new byte[1024];
+			while ((length = input.read(buffer)) > 0) {
+				digest.update(buffer, 0, length);
+			}
+			input.close();
+
+			byte[] checksum = digest.digest();
+
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < checksum.length; i++) {
+				builder.append(Integer.toString((checksum[i] & 0xff) + 0x100, 16).substring(1));
+			}
+
+			return builder.toString();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		return null;
+	}
+
+	private int delete(File file) {
+		int deleted = 0;
+		if (file.isDirectory()) {
+			for (File local : file.listFiles()) {
+				deleted += delete(local);
+			}
+		}
+		file.delete();
+
+		return (deleted += 1);
+	}
+
+	public void reset() {
+		if (hashes.size() > 0) {
+			hashes.clear();
+		}
+		this.deleted = 0;
+	}
+
+	public int getDeleted() {
+		return deleted;
+	}
+}
